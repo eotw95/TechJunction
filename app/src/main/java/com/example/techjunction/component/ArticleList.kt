48780fb719ada227b5ad2011fde85223417a2b2e
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-09-12 18:58:08 +0900

    Add result observer of api request

--- app/src/main/java/com/example/techjunction/component/ArticleList.kt
+++ app/src/main/java/com/example/techjunction/component/ArticleList.kt
@@ -1,22 +1,25 @@
 package com.example.techjunction.component
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.techjunction.viewmodel.QiitaArticlesViewModel
 
 @Composable
 fun ArticleList(){
+    val vm: QiitaArticlesViewModel = viewModel()
+    val observeQiitaArticles = vm.articles.observeAsState()
     Column {
-        val dummyList = List(5){
-            "sampleArticle"
-        }
-        dummyList.forEach { item ->
-            Text(text = item)
+        observeQiitaArticles.value?.forEach { article ->
+            Text(text = article.title)
             Divider()
             Spacer(modifier = Modifier.padding(vertical = 10.dp))
         }
