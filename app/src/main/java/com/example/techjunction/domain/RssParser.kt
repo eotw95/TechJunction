fb30ce37c39704a3432fe9f3179e1cc26608a380
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-10-19 21:46:25 +0900

    Update RssParser

--- app/src/main/java/com/example/techjunction/domain/RssParser.kt
+++ app/src/main/java/com/example/techjunction/domain/RssParser.kt
@@ -1,11 +1,35 @@
 package com.example.techjunction.domain
 
 import com.example.techjunction.room.RssRepository
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
 import java.io.InputStream
 
 class RssParser(private val repo: RssRepository) {
+    companion object {
+        const val START_TAG = XmlPullParser.START_TAG
+        const val TEXT = XmlPullParser.TEXT
+        const val END_TAG = XmlPullParser.END_TAG
+        const val END_DOCUMENT = XmlPullParser.END_DOCUMENT
+    }
 
-    suspend fun parse(input: InputStream, url: String) {
-        // Todo: fileのXMLを解析
+    suspend fun parse(input: InputStream, rssUrl: String) {
+        val parser = XmlPullParserFactory.newInstance().newPullParser()
+        parser.setInput(input, null)
+        var event = parser.eventType
+        while (event != END_DOCUMENT) {
+            when (event) {
+                START_TAG -> {
+
+                }
+                TEXT -> {
+
+                }
+                END_TAG -> {
+
+                }
+            }
+            event = parser.next()
+        }
     }
 }
