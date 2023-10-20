302747a30e317ba8b7884bc812b16628d71bce67
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-10-20 23:17:54 +0900

    Update RdfTag

--- app/src/main/java/com/example/techjunction/domain/RdfTag.kt
+++ app/src/main/java/com/example/techjunction/domain/RdfTag.kt
@@ -1,6 +1,26 @@
 package com.example.techjunction.domain
 
 class RdfTag(name: String): Tag(name) {
+
+    override fun createChildTag(name: String): Tag {
+        return when(name) {
+            "channel" -> RdfChannelTag(name)
+            "item" -> RdfItemTag(name)
+            else -> super.createChildTag(name)
+        }
+    }
+    override fun handleChildTagEnd() {
+        TODO("Not yet implemented")
+    }
+}
+
+private class RdfChannelTag(name: String): Tag(name) {
+    override fun handleChildTagEnd() {
+        TODO("Not yet implemented")
+    }
+}
+
+private class RdfItemTag(name: String): Tag(name) {
     override fun handleChildTagEnd() {
         TODO("Not yet implemented")
     }
