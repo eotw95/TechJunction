5ffb9272ea0aebd8666b7c5cdd16e028bff99ac5
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-10-20 23:22:56 +0900

    Update RssTag

--- app/src/main/java/com/example/techjunction/domain/RssTag.kt
+++ app/src/main/java/com/example/techjunction/domain/RssTag.kt
@@ -1,6 +1,32 @@
 package com.example.techjunction.domain
 
 class RssTag(name: String): Tag(name) {
+
+    override fun createChildTag(name: String): Tag {
+        return when(name) {
+            "channel" -> RssChannelTag(name)
+            else -> super.createChildTag(name)
+        }
+    }
+    override fun handleChildTagEnd() {
+        TODO("Not yet implemented")
+    }
+}
+
+private class RssChannelTag(name: String): Tag(name) {
+
+    override fun createChildTag(name: String): Tag {
+        return when(name) {
+            "item" -> RssItemTag(name)
+            else -> super.createChildTag(name)
+        }
+    }
+    override fun handleChildTagEnd() {
+        TODO("Not yet implemented")
+    }
+}
+
+private class RssItemTag(name: String): Tag(name) {
     override fun handleChildTagEnd() {
         TODO("Not yet implemented")
     }
