462c2603465c224457d7f816cf8a4850352ee126
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-10-22 11:21:26 +0900

    Fix RdfTag

--- app/src/main/java/com/example/techjunction/domain/RdfTag.kt
+++ app/src/main/java/com/example/techjunction/domain/RdfTag.kt
@@ -5,70 +5,72 @@ import com.example.techjunction.room.RssItem
 import com.example.techjunction.room.RssRepository
 import java.util.Date
 
-open class RdfTag(name: String, val rssUrl: String): Tag(name) {
+class RdfTag(name: String, val rssUrl: String): Tag(name) {
 
     override fun createChildTag(name: String): Tag {
         return when(name) {
-            "channel" -> RdfChannelTag(name, rssUrl)
-            "item" -> RdfItemTag(name, rssUrl)
+            "channel" -> RdfChannelTag(name)
+            "item" -> RdfItemTag(name)
             else -> super.createChildTag(name)
         }
     }
     override suspend fun handleChildTagEnd(tag: Tag, repo: RssRepository, date: Date) {
         when(tag) {
-            is RdfChannelTag -> repo.insertOrUpdateChannel(tag.asDatabaseModel(tag))
+            is RdfChannelTag -> repo.insertOrUpdateChannel(
+                rssUrl,
+                tag.title,
+                tag.desc,
+                tag.link,
+                date
+            )
             is RdfItemTag -> repo.insertOrUpdateItem(tag.asDatabaseModel(tag))
         }
     }
 }
 
-private class RdfChannelTag(name: String, rssUrl: String): RdfTag(name, rssUrl) {
+private class RdfChannelTag(name: String): Tag(name) {
     var title: String = ""
     var desc: String = ""
     var link: String = ""
-    var latestDate: Date = Date()
     override suspend fun handleChildTagEnd(tag: Tag, repo: RssRepository, date: Date) {
         when(tag.name) {
             "title" -> title = tag.text
             "desc" -> desc = tag.text
             "link" -> link = tag.text
         }
-        latestDate = date
     }
 }
 
-private class RdfItemTag(name: String, rssUrl: String): RdfTag(name, rssUrl) {
+private class RdfItemTag(name: String): Tag(name) {
     var title: String = ""
     var desc: String = ""
     var link: String = ""
-    var latestDate: Date = Date()
     override suspend fun handleChildTagEnd(tag: Tag, repo: RssRepository, date: Date) {
         when(tag.name) {
             "title" -> title = tag.text
             "desc" -> desc = tag.text
             "link" -> link = tag.text
         }
-        latestDate = date
     }
 }
 
-private fun RdfChannelTag.asDatabaseModel(tag: RdfChannelTag):  RssChannel{
-    return RssChannel(
-        id = 0,
-        rssUrl = tag.rssUrl,
-        title = tag.title,
-        description = tag.desc,
-        link = tag.link,
-        latestDate = tag.latestDate.time
-    )
-}
-
-private fun RdfItemTag.asDatabaseModel(tag: RdfItemTag):  RssItem{
-    return RssItem(
-        id = 0,
-        title = tag.title,
-        description = tag.desc,
-        link = tag.link,
-        pubDate = tag.latestDate.time
-    )
-}
+//private fun RdfChannelTag.asDatabaseModel(tag: RdfChannelTag):  RssChannel{
+//    return RssChannel(
+//        id = 0,
+//        rssUrl = tag.rssUrl,
+//        title = tag.title,
+//        description = tag.desc,
+//        link = tag.link,
+//        latestDate = tag.latestDate.time
+//    )
+//}
+//
+//private fun RdfItemTag.asDatabaseModel(tag: RdfItemTag):  RssItem{
+//    return RssItem(
+//        id = 0,
+//        title = tag.title,
+//        description = tag.desc,
+//        link = tag.link,
+//        pubDate = tag.latestDate.time
+//    )
+//}
