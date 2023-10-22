1fdb1bcb657238ad42514af5a676c6efe068e837
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-10-22 17:31:47 +0900

    Update RssTag

--- app/src/main/java/com/example/techjunction/domain/RssTag.kt
+++ app/src/main/java/com/example/techjunction/domain/RssTag.kt
@@ -1,21 +1,39 @@
 package com.example.techjunction.domain
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.techjunction.room.RssRepository
+import com.example.techjunction.util.DateConverter
+import java.util.Date
 
-class RssTag(name: String): Tag(name) {
+class RssTag(name: String, private val rssUrl: String): Tag(name) {
 
     override fun createChildTag(name: String): Tag {
         return when(name) {
-            "channel" -> RssChannelTag(name)
+            "channel" -> RssChannelTag(name, rssUrl)
             else -> super.createChildTag(name)
         }
     }
-    override fun handleChildTagEnd(tag: Tag, repo: RssRepository) {
-        TODO("Not yet implemented")
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override suspend fun handleChildTagEnd(tag: Tag, repo: RssRepository, date: Date) {
+        if (tag is RssChannelTag) {
+            repo.insertOrUpdateItem(
+                rssUrl,
+                tag.title,
+                tag.desc,
+                tag.link,
+                DateConverter.asDate(tag.lastBuildDate)
+            )
+        }
     }
 }
 
-private class RssChannelTag(name: String): Tag(name) {
+private class RssChannelTag(name: String, private val channelLink: String): Tag(name) {
+    var title = ""
+    var desc = ""
+    var link = ""
+    var lastBuildDate = ""
 
     override fun createChildTag(name: String): Tag {
         return when(name) {
@@ -23,13 +41,39 @@ private class RssChannelTag(name: String): Tag(name) {
             else -> super.createChildTag(name)
         }
     }
-    override fun handleChildTagEnd(tag: Tag, repo: RssRepository) {
-        TODO("Not yet implemented")
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override suspend fun handleChildTagEnd(tag: Tag, repo: RssRepository, date: Date) {
+        if (tag is RssItemTag) {
+            repo.insertOrUpdateChannel(
+                channelLink,
+                tag.title,
+                tag.desc,
+                tag.link,
+                DateConverter.asDate(tag.pubDate)
+            )
+        } else {
+            when(tag.name) {
+                "title" -> title = tag.text
+                "desc" -> desc = tag.text
+                "link" -> link = tag.text
+                "lastBuildDate" -> lastBuildDate = tag.text
+            }
+        }
     }
 }
 
 private class RssItemTag(name: String): Tag(name) {
-    override fun handleChildTagEnd(tag: Tag, repo: RssRepository) {
-        TODO("Not yet implemented")
+    var title = ""
+    var desc = ""
+    var link = ""
+    var pubDate = ""
+    override suspend fun handleChildTagEnd(tag: Tag, repo: RssRepository, date: Date) {
+        when(tag.name) {
+            "title" -> title = tag.text
+            "desc" -> desc = tag.text
+            "link" -> link = tag.text
+            "pubDate" -> pubDate = tag.text
+        }
     }
 }
