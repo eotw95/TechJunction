88dbbfe4eda77d35498c9496079b406d314152d2
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-11-01 23:20:51 +0900

    Add calling process temporarily

--- app/src/main/java/com/example/techjunction/MainActivity.kt
+++ app/src/main/java/com/example/techjunction/MainActivity.kt
@@ -16,6 +16,8 @@ import com.example.techjunction.worker.RssDownloadWorker
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        RssDownloadWorker.start(this)
+        Thread.sleep(10000)
         setContent {
             TechJunctionTheme {
                 // A surface container using the 'background' color from the theme
@@ -23,10 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    HomeScreen()
-                    Button(onClick = { RssDownloadWorker.start(this@MainActivity) }) {
-                        Text(text = "start")
-                    }
+                    HomeScreen()
                 }
             }
         }
