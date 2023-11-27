4912dacc7927fdc854e7c787332ce986d1e8ecf4
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-11-27 22:59:22 +0900

    Add font

--- app/src/main/java/com/example/techjunction/screens/component/Header.kt
+++ app/src/main/java/com/example/techjunction/screens/component/Header.kt
@@ -2,10 +2,12 @@ package com.example.techjunction.screens.component
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -22,8 +24,7 @@ fun Header() {
         Text(
             text = APP_NAME,
             textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Black,
-            fontSize = 30.sp
+            style = MaterialTheme.typography.displayLarge
         )
     }
 }
--- app/src/main/java/com/example/techjunction/ui/theme/Type.kt
+++ app/src/main/java/com/example/techjunction/ui/theme/Type.kt
@@ -2,12 +2,23 @@ package com.example.techjunction.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.techjunction.R
+
+val AbrilFatface = FontFamily(
+    Font(R.font.abril_fatface_regular)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = AbrilFatface,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp
+    ),
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
