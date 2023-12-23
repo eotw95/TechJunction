41c57e6c8ed022816ed0e1b5039d607c71c63d87
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-12-23 10:07:26 +0900

    Add BottomNavigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,9 @@ dependencies {
     // navigation
     implementation ("androidx.navigation:navigation-compose:2.4.2")
 
+    // bottom navigation
+    implementation ("androidx.compose.material:material:1.3.1")
+
     // coil
     implementation ("io.coil-kt:coil-compose:2.1.0")
 
--- app/src/main/java/com/example/techjunction/MainActivity.kt
+++ app/src/main/java/com/example/techjunction/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.techjunction
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.techjunction.screens.component.MainNavHost
+import com.example.techjunction.screens.ArticleDetailScreen
+import com.example.techjunction.screens.HomeScreen
 import com.example.techjunction.ui.theme.TechJunctionTheme
 import com.example.techjunction.worker.RssDownloadWorker
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +44,70 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val navController = rememberNavController()
-                    MainNavHost(navController = navController)
+                    val items = listOf(
+                        Screen.ArticlesAll,
+                        Screen.Channel
+                    )
+                    Scaffold(
+                        bottomBar = {
+                            BottomNavigation {
+                                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                                val currentDestination = navBackStackEntry?.destination
+                                items.forEach { screen ->
+                                    BottomNavigationItem(
+                                        label = { Text(text = stringResource(id = screen.resourceId)) },
+                                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                                        icon = { Icon(imageVector = Icons.Filled.AccountCircle, contentDescription = null) },
+                                        onClick = {
+                                                  navController.navigate(screen.route) {
+                                                      popUpTo(navController.graph.findStartDestination().id) {
+                                                          saveState = true
+                                                      }
+                                                      launchSingleTop = true
+                                                      restoreState = true
+                                                  }
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                    ) {
+                        NavHost(
+                            navController = navController,
+                            startDestination = Screen.ArticlesAll.route,
+                            builder = {
+                                composable(Screen.ArticlesAll.route) {
+                                    HomeScreen(
+                                        onClick = { url ->
+                                            navController.navigate("detail/$url")
+                                        }
+                                    )
+                                }
+                                composable("channel") {
+
+                                }
+                                composable("detail/{url}") { navBackStackEntry ->
+                                    navBackStackEntry.arguments?.getString("url")?.let {
+                                        ArticleDetailScreen(
+                                            url = it,
+                                            onClick = { navController.navigateUp() }
+                                        )
+                                    }
+                                }
+                            }
+                        )
+                    }
                 }
             }
         }
     }
+}
+
+sealed class Screen(
+    val route: String,
+    val resourceId: Int
+) {
+    object ArticlesAll: Screen("articlesAll", R.string.articlesAll)
+    object Channel: Screen("channel", R.string.channel)
+    object Detail: Screen("detail", R.string.detail)
 }
