33b00a3cafdb75de001158ec2e0381091fed22ae
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2024-02-01 23:16:44 +0900

    Fix bug that delete icon is not displayed

--- app/src/main/java/com/example/techjunction/screens/FollowArticles.kt
+++ app/src/main/java/com/example/techjunction/screens/FollowArticles.kt
@@ -55,7 +55,7 @@ fun FollowArticles(
         Column(
             modifier = Modifier
                 .verticalScroll(rememberScrollState())
-                .padding(start = 15.dp, top = 10.dp, end = 15.dp, bottom = 70.dp ),
+                .padding(top = 10.dp, bottom = 70.dp ),
         ) {
             viewModel?.fetchFollowArticles()
             followArticleObserver?.value?.forEach { article ->
